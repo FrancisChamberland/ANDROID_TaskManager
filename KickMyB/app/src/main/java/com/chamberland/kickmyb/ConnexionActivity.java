e36cb812ffc264169594c03a660a09cc38e7b71f
Francis Chamberland <1670240@cegepmontpetit.ca>
2022-01-26 20:01:51 -0500

    FCT Configuration binding
    Configuration du data binding et view binding

--- KickMyB/app/src/main/java/com/chamberland/kickmyb/ConnexionActivity.java
+++ KickMyB/app/src/main/java/com/chamberland/kickmyb/ConnexionActivity.java
@@ -3,12 +3,20 @@ package com.chamberland.kickmyb;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.chamberland.kickmyb.databinding.ActivityConnexionBinding;
 
 public class ConnexionActivity extends AppCompatActivity {
+    private ActivityConnexionBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_connexion);
+        binding = ActivityConnexionBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        this.setTitle("Connexion");
     }
 }
