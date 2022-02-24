02f69bd3ff29211e4573f6c69fcdb0d8c4139793
Francis <1670240@cegepmontpetit.ca>
2022-02-24 17:40:48 -0500

    FCT Signup request
    RegisterActivity permet d'envoyer une requête SignupRequest au serveur et de recevoir une réponse réussie

--- KickMyB/app/src/main/java/com/chamberland/kickmyb/activities/RegisterActivity.java
+++ KickMyB/app/src/main/java/com/chamberland/kickmyb/activities/RegisterActivity.java
@@ -2,14 +2,26 @@ package com.chamberland.kickmyb.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.chamberland.kickmyb.databinding.ActivityRegisterBinding;
+import com.chamberland.kickmyb.http.RetrofitUtil;
+import com.chamberland.kickmyb.http.Service;
+
+import org.kickmyb.transfer.SigninRequest;
+import org.kickmyb.transfer.SigninResponse;
+import org.kickmyb.transfer.SignupRequest;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class RegisterActivity extends AppCompatActivity {
     private ActivityRegisterBinding binding;
+    private Service service;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +29,7 @@ public class RegisterActivity extends AppCompatActivity {
         binding = ActivityRegisterBinding.inflate(getLayoutInflater());
         View view = binding.getRoot();
         setContentView(view);
-
+        service = RetrofitUtil.get();
         this.setTitle("Inscription");
         createEventsListeners();
     }
@@ -28,8 +40,34 @@ public class RegisterActivity extends AppCompatActivity {
             startActivity(i);
         });
         binding.btnRegister.setOnClickListener(v -> {
-            Intent i = new Intent(RegisterActivity.this, HomeActivity.class);
-            startActivity(i);
+            String username = String.valueOf(binding.inputUsername.getEditText().getText());
+            String password = String.valueOf(binding.inputPassword.getEditText().getText());
+            String confirmPassword = String.valueOf(binding.inputConfirmPassword.getEditText().getText());
+
+            if (!password.equals(confirmPassword)) return;
+
+            SignupRequest signupRequest = new SignupRequest();
+            signupRequest.username = username;
+            signupRequest.password = password;
+
+            service.signup(signupRequest).enqueue(new Callback<SigninResponse>() {
+                @Override
+                public void onResponse(Call<SigninResponse> call, Response<SigninResponse> response) {
+                    if (response.isSuccessful()){
+                        Log.i("SIGNUP", "Response is successful");
+                        Intent i = new Intent(RegisterActivity.this, HomeActivity.class);
+                        startActivity(i);
+                    }
+                    else{
+                        Log.e("SIGNUP", "Response is not successful");
+                    }
+                }
+
+                @Override
+                public void onFailure(Call<SigninResponse> call, Throwable t) {
+                    Log.e("SIGNUP", "Request failed");
+                }
+            });
         });
     }
 }
---
+++ KickMyB/app/src/main/java/com/chamberland/kickmyb/http/Service.java
@@ -0,0 +1,13 @@
+package com.chamberland.kickmyb.http;
+
+import org.kickmyb.transfer.SigninResponse;
+import org.kickmyb.transfer.SignupRequest;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface Service {
+    @POST("/api/id/signup")
+    Call<SigninResponse> signup(@Body SignupRequest signupRequest);
+}
